4ad0a977907be4a1a9d1e78c187b8352d2cab444
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-07-21 00:42:14 +0100

    hllo world  am  back

---
+++ server/index.js
@@ -0,0 +1,3 @@
+require('dotenv').config()
+console.log(process.env) // remove this after you've confirmed it is workingrequire('dotenv').config()
+console.log(process.env.HELLO) // remove this after you've confirmed it is working

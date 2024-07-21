e3cd37698b7e8fcf7db1801dea849a59685fb479
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-07-21 01:17:26 +0100

    here we go

---
+++ client/src/components/Mesage.jsx
@@ -0,0 +1,19 @@
+
+
+
+
+function Mesage(){
+
+return(
+    <>
+        
+        <div className="w-52 bg-yellow-100 p-4 rounded-sm ">
+            <h1>anis</h1>
+            <h3>pourquoi la vie ?</h3>
+        </div>
+    
+    </>
+)
+
+}
+export default Mesage
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "client/",
+    element: <App/>,
+    children:[
+      {
+        path: "newmessaage",
+        element: <h3>en aattentte</h3>
+      }
+    ]
+  },
+  
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )

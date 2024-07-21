91a08dee4d998c5bdb1a99f2549d1cd0e49038b2
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-07-21 01:44:37 +0100

    delulu is the sollulu

---
+++ client/src/components/NewMesage.jsx
@@ -0,0 +1,13 @@
+function NewMesage(){
+
+    return(
+        <>
+            <div className=" bg-white/70 w-full min-h-svh fixed flex flex-col items-center justify-center">
+                
+            </div>  
+        </>
+        
+    )
+
+}
+export default NewMesage

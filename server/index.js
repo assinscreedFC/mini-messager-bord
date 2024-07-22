e3b146025f89c3ddfbb58a0dceb42a0a5c5c40e8
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-07-22 14:00:12 +0100

    go

--- server/index.js
+++ server/index.js
@@ -1,3 +1,32 @@
-require('dotenv').config()
-console.log(process.env) // remove this after you've confirmed it is workingrequire('dotenv').config()
-console.log(process.env.HELLO) // remove this after you've confirmed it is working
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+// const workoutRoutes = require('./routes/workouts'); // Décommente cette ligne si tu as ce fichier
+
+// express app
+const app = express();
+
+
+// middleware
+
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+  })
+
+// routes
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to database');
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log('Error connecting to the database:', err);
+  });

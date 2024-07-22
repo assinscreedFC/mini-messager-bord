7d5e5f71d4488ce63fa68520021117910ae7af8c
ANIS <119749586+assinscreedFC@users.noreply.github.com>
2024-07-22 22:07:39 +0100

    just for  fun switch  too pg

--- server/index.js
+++ server/index.js
@@ -4,28 +4,28 @@ const express = require('express');
 const mongoose = require('mongoose');
 // const workoutRoutes = require('./routes/workouts'); // Décommente cette ligne si tu as ce fichier
 
-// express app
+// express ap
 const app = express();
 
 
 // middleware
-
+app.use(express.json())
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
   })
 
-// routes
+// rout
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('connected to database');
     // listen to port
     app.listen(process.env.PORT, () => {
-      console.log('listening for requests on port', process.env.PORT);
-    });
+      console.log('listening for requests on port', process.env.PORT); });
+   
   })
   .catch((err) => {
     console.log('Error connecting to the database:', err);
